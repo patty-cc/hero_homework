0e04a7f7be8614c1ca79a294175065c2cc43d436
Iain Paterson <iain.paterson.cc@gmail.com>
2017-10-15 12:55:09 +0100

    Add Food replenishment method

--- hero.js
+++ hero.js
@@ -6,6 +6,13 @@ var Hero = function(name, healthValue, favouriteFood ){
   this.talk = function(){
     return "My name is Link";
   };
+
+  Hero.prototype.eatFood = function ( food ) {
+    if( this.favouriteFood === food.name){
+      return this.healthValue += (food.replenValue * 1.5);
+    }
+    return this.healthValue += food.replenValue;
+  };
 };
 
 module.exports = Hero;
--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -1,12 +1,16 @@
 var assert = require('assert');
-var Hero = require('../hero.js')
+var Hero = require('../hero.js');
+var Food = require('../food.js');
 
 describe('Hero', function() {
 
 var hero;
+var food;
 
   beforeEach( function() {
     hero = new Hero( 'Link', 100, 'Apple');
+    foodA = new Food( 'Chocolate');
+    foodB = new Food( 'Crisps');
   });
 
   it('should have a name', function() {
@@ -23,9 +27,21 @@ var hero;
 
   it('should have an empty task array', function() {
     assert.strictEqual(hero.tasks.length, 0 );
-  })
+  });
 
   it('should be able to talk and say their name', function() {
     assert.strictEqual(hero.talk(), "My name is Link");
-  })
+  });
+
+  it('should replenish energy when eating food', function() {
+    var newHero = new Hero( 'Solid Snake', 80, 'Crisps');
+    newHero.eatFood( foodA);
+    assert.strictEqual(newHero.healthValue, 90);
+  });
+
+  it('should replenish extra energy if food is favourite', function() {
+    var newHero = new Hero('Solid Snake', 85, 'Crisps');
+    newHero.eatFood( foodB );
+    assert.strictEqual(newHero.healthValue, 100);
+  });
 })
